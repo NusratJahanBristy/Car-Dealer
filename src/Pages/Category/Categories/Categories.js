dd99cfae09858177e81f0dffd8181346e9a9e74c
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-11-28 17:20:14 +0600

    products loaded

--- src/Pages/Category/Categories/Categories.js
+++ src/Pages/Category/Categories/Categories.js
@@ -15,8 +15,9 @@ const Categories = () => {
     }, [])
     return (
         <div>
+             {/* <div>{products.length}</div> */}
             <div className='text-center mb-2'>
-                <h2 className='text-5xl font-semibold text-orange-600'>Our Services</h2>
+                <h2 className='text-5xl font-semibold text-orange-600'>Our Products</h2>
                 <p>Orders your dreamy & desire Car..</p>
             </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
--- src/Pages/Category/CategoriesItem/CategoriesItem.js
+++ src/Pages/Category/CategoriesItem/CategoriesItem.js
@@ -6,37 +6,40 @@ import 'react-photo-view/dist/react-photo-view.css';
 
 
 const CategoriesItem = ({ product }) => {
-  const { _id, img, } = product;
+  const { _id, img,originalPrice,resalePrice,yearsOfUse,rating,servicesName,sellersName,condition,details,location,lastModified} = product;
   return (
-    <div>{product.length}</div>
+    // <div>{product.length}</div>
    
 
-    // <div className="card h-5/6 w-full bg-base-100 shadow-xl fixed-height">
-    //    <PhotoProvider>
-    //    <PhotoView src={img}>
-    //    <figure className='h-1/2'><img className='h-5/6 w-5/6' src={img} alt="Shoes" /></figure>
-    //    </PhotoView>
+    <div className="card h-5/6 w-full bg-base-100 shadow-xl fixed-height">
+       <PhotoProvider>
+       <PhotoView src={img}>
+       <figure className='h-1/2'><img className='h-5/6 w-5/6' src={img} alt="Shoes" /></figure>
+       </PhotoView>
      
-    //   <div className="card-body">
-    //     <h2 className="card-title text-3xl font-semibold">name</h2>
-    //     <p className='text-xl'>
-    //       {/* {
-    //         details?.length > 100 ?
-    //           <p>{details?.slice(0, 100) + '...'}</p>
-    //           : <p>{details}</p>
-    //       } */}
-    //     </p>
-    //     <div className='flex'>
-    //       <p className='text-2xl text-orange-600 font-semibold'>Price: $5</p>
-    //       <p className='text-2xl text-gray-600 font-semibold'><div className='flex justify-end text-warning align-top'>5<FaStar></FaStar><FaStar></FaStar><FaStarHalfAlt></FaStarHalfAlt> </div></p>
-    //     </div>
+      <div className="card-body">
+        <h2 className="card-title text-3xl font-semibold">name</h2>
+        <p>kjf</p>
+        <p className='text-xl'>
+          {
+            details?.length > 100 ?
+              <p>{details?.slice(0, 100) + '...'}</p>
+              : <p>{details}</p>
+          }
+        </p>
+        <div className='flex'>
+          <p className='text-2xl text-orange-600 font-semibold'>OriginalPrice: ${originalPrice}</p>
+          <p className='text-2xl text-orange-600 font-semibold'>ResalePrice: ${resalePrice}</p>
+          <p className='text-2xl text-orange-600 font-semibold'>YearsOfUse: {yearsOfUse}y</p>
+          <p className='text-2xl text-gray-600 font-semibold'><div className='flex justify-end text-warning align-top'>5<FaStar></FaStar><FaStar></FaStar><FaStarHalfAlt></FaStarHalfAlt> </div></p>
+        </div>
 
-    //     <div className="card-actions justify-end">
-    //       {/* <button className="btn btn-block btn-warning bg-orange-600"><Link className='text-white  text-decoration-none' to={`/services/${_id}`}>Go to Details</Link></button> */}
-    //     </div>
-    //   </div>
-    //   </PhotoProvider>
-    // </div>
+        <div className="card-actions justify-end">
+          {/* <button className="btn btn-block btn-warning bg-orange-600"><Link className='text-white  text-decoration-none' to={`/services/${_id}`}>Go to Details</Link></button> */}
+        </div>
+      </div>
+      </PhotoProvider>
+    </div>
   );
 };
 
