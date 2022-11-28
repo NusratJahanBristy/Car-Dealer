90269a20e9d26de7093790566446555e61c161d7
NusratJahanBristy <hellonusratjahanbristy@gmail.com>
2022-11-28 23:44:23 +0600

    all user updated

--- src/Pages/Dashboard/Dashboard/AllUser/AllUser.js
+++ src/Pages/Dashboard/Dashboard/AllUser/AllUser.js
@@ -30,7 +30,7 @@ const AllUsers = () => {
 
   return (
     <div>
-      <h2 className="text-3xl">All Users</h2>
+  
       <div className="overflow-x-auto">
         <table className="table w-full">
           <thead>
